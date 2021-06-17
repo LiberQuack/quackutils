dc5c045263cc67c140ab70caaefa349801d0ecaa
Quack Martins <rogue.thiago@gmail.com>
2021-06-17 19:01:52 -0300

    Implement initial evolution report

--- dictionary.ts
+++ dictionary.ts
@@ -11,3 +11,10 @@ export function listToDictionary<T>(list: T[], key: keyof T): Dictionary<T> {
 export function dictionaryToList<T>(dict: Dictionary<T>): T[] {
     return Object.keys(dict).map(key => dict[key]);
 }
+
+export function dictionaryEach<T,Y>(dict: Dictionary<T>, acc: Y, reducer: (acc:Y, item:T) => Y): Y {
+    const result = Object.keys(dict).reduce((reducAcc, key) => {
+        return reducer(reducAcc, dict[key]);
+    }, acc);
+    return result;
+}
