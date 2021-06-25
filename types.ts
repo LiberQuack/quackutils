0edde79b6e5d654e44b6eb82687ffadbd281ae6b
Quack Martins <rogue.thiago@gmail.com>
2021-06-25 20:09:29 -0300

    Add partial field for custody when no price is found for the ticker

---
+++ types.ts
@@ -0,0 +1,37 @@
+export type Dictionary<T> = {
+    [x: string]: T;
+};
+
+export type UnPartial<T> = T extends Partial<infer U> ? U : never
+export type UnArray<T> = T extends Array<infer U> ? U : never
+
+type NativeType = Date | number | string | boolean | undefined
+
+export type Pojo = {
+    [x: string]: NativeType | NativeType[] | Pojo | Pojo[]
+}
+
+//PWA apis
+declare global {
+    interface Navigator {
+        getInstalledRelatedApps(): Promise<{
+            platform: "webapp"
+            url: string
+        }[]>
+    }
+}
+
+export interface BeforeInstallPromptEvent extends Event {
+    readonly platforms: string[];
+    readonly userChoice: Promise<{
+        outcome: "accepted" | "dismissed";
+        platform: string;
+    }>;
+    prompt(): Promise<void>;
+}
+
+declare global {
+    interface WindowEventMap {
+        beforeinstallprompt: BeforeInstallPromptEvent;
+    }
+}
