639a7f543cd4cf031ae455c31100c39587e1848d
Quack Martins <rogue.thiago@gmail.com>
2021-07-09 18:50:35 -0300

    Implement proportions chart

--- dictionary.ts
+++ dictionary.ts
@@ -1,4 +1,6 @@
-import {Dictionary} from "./types";
+export type Dictionary<T> = {
+    [x: string]: T;
+};
 
 export function listToDictionary<T>(list: T[], key: keyof T): Dictionary<T> {
     return list.reduce((acc, item) => {
@@ -8,4 +10,22 @@ export function listToDictionary<T>(list: T[], key: keyof T): Dictionary<T> {
 
 export function dictionaryToList<T>(dict: Dictionary<T>): T[] {
     return Object.keys(dict).map(key => dict[key]!);
+}
+
+export function listToDictionaryAcc<T>(list: T[], key: keyof T): Dictionary<T[]> {
+    let acc = {} as any;
+
+    for (let item of list) {
+        let previousValue = acc[item[key] as any] as T[] | undefined
+        acc[item[key] as any] = previousValue ? [...previousValue, item] : [item]
+    }
+
+    return acc;
+}
+
+export function dictionaryAcc<T,Y>(dict: Dictionary<T>, acc: Y, reducer: (acc:Y, item:T) => Y): Y {
+    const result = Object.keys(dict).reduce((reducAcc, key) => {
+        return reducer(reducAcc, dict[key]!);
+    }, acc);
+    return result;
 }
