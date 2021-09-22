08a0c7f36f28ebf4def8fb646bb9fcfac5b9bd08
Quack Martins <rogue.thiago@gmail.com>
2021-09-22 04:10:07 -0300

    Improve payment-stripe-provider types

--- payment/providers/payment-stripe-provider.ts
+++ payment/providers/payment-stripe-provider.ts
@@ -4,8 +4,8 @@ import {Stripe} from "stripe";
 
 export type PaymentStripeAccount = EnforcePaymentProviderBase<{
     provider: "stripe",
-    customerId: string,
-    paymentSources: Array<Stripe.Source.Card>
+    customer: Stripe.Customer
+    paymentSources: Array<Stripe.Card>
 }>
 
 export class PaymentStripeProvider implements PaymentAccountProvider, SubscriptionProvider {
@@ -20,9 +20,9 @@ export class PaymentStripeProvider implements PaymentAccountProvider, Subscripti
     async createCard(user: UserPaymentAccount, data: Stripe.Token): Promise<PaymentStripeAccount> {
         const stripeAccount = await this._extractOrCreateStripeAccount(user);
 
-        const sourceListResponse = await this.stripe.customers.listSources(stripeAccount.customerId, {object: 'card'});
-        const currentCardSources = sourceListResponse.data as Stripe.Source.Card[];
-        const createdSource = await this.stripe.customers.createSource(stripeAccount.customerId, {source: data.id}) as Stripe.Source.Card;
+        const sourceListResponse = await this.stripe.customers.listSources(stripeAccount.customer.id, {object: 'card'});
+        const currentCardSources = sourceListResponse.data as Stripe.Card[];
+        const createdSource = await this.stripe.customers.createSource(stripeAccount.customer.id, {source: data.id}) as Stripe.Card;
 
         stripeAccount.paymentSources = [
             createdSource,
@@ -45,8 +45,8 @@ export class PaymentStripeProvider implements PaymentAccountProvider, Subscripti
 
     private async _createStripeAccount(user: UserPaymentAccount): Promise<PaymentStripeAccount> {
         const customerResponse = await this.stripe.customers.create({email: user.email});
-        const customerId = customerResponse.id;
-        const stripeAccount: PaymentStripeAccount = {provider: this.provider, customerId: customerId, paymentSources: []};
+        const {lastResponse, ...customer} = customerResponse
+        const stripeAccount: PaymentStripeAccount = {provider: this.provider, customer: customer, paymentSources: []};
         return stripeAccount;
     }
 
