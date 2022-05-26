99f11dfcd53156a396227fc7bbe3b3feb2d3dcb0
Quack Martins <rogue.thiago@gmail.com>
2022-05-26 16:54:51 -0500

    WIP: Implementing timer-element

---
+++ ui/components/timer-element/timer-element.tsx
@@ -0,0 +1,47 @@
+import {CustomElement} from "../../ui-types";
+import {component, useMemo} from "haunted";
+import {html} from "lit";
+import {useEffect, useState} from "haunted/lib/core";
+import {customElement} from "lit/decorators";
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+export const TimerElement:CustomElement<{targetDate: string}> = ({targetDate}) => {
+
+    const initialDiff = useMemo(() => targetDate && fmtTime(calcTimeDiff(targetDate)), [])
+    const [time, setTime] = useState(initialDiff || "00:00:00");
+    const [lastInterval, setLastInterval] = useState(0);
+
+    const [hours, minutes, seconds] = time.split(":");
+
+    useEffect(() => {
+        window.clearInterval(lastInterval);
+        setInterval(() => {
+            if (targetDate) {
+                setTime(fmtTime(calcTimeDiff(targetDate)));
+            }
+        }, 1000);
+    }, [targetDate])
+
+    return html`<span>${hours}</span>:<span>${minutes}</span>:<span>${seconds}</span>`
+}
+
+function calcTimeDiff(targetDate: string) {
+    const timeNow = Date.now();
+    const targetDatee = new Date(targetDate);
+    const target = targetDatee.getTime() + (targetDatee.getTimezoneOffset() * minute);
+    const diff = target - timeNow;
+    return diff
+}
+
+function fmtTime(time: number) {
+    const hours = Math.floor(time / hour).toString();
+    const minutes = Math.floor((time % hour) / minute).toString();
+    const seconds = Math.floor((time % minute) / second).toString();
+    return `${hours}:${minutes.padStart(2, "0")}:${seconds.padStart(2, "0")}`
+}
+
+customElements.define("timer-element", component(TimerElement, {observedAttributes: ["target-date"] as any}));
