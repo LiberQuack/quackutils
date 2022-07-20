5632aba04373546664d8a712b413d20b0424eadd
LiberQuack <LiberQuack@users.noreply.github.com>
2022-07-20 18:06:15 +0000

    deploy: 0e0c7da94c21d7a48bcfd71d20f44610f26a5994

---
+++ stripe-form.df73c490.js
@@ -0,0 +1,16 @@
+var e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire9622;e.register("2f6Jz",(function(t,r){var i,n,p,d,s;i=t.exports,Object.defineProperty(i,"__esModule",{value:!0,configurable:!0}),n=t.exports,p="default",d=()=>b,Object.defineProperty(n,p,{get:d,set:s,enumerable:!0,configurable:!0}),e("l0AYr");var o=e("di58i"),m=e("dlCTC"),l=e("jkSQt");e("5K2nE");const u=l.html`
+    <stripe-form stripe-public-key="pk_test_51IG332FIx1iLyZGc71gYaVCUDKMPAFlRn49tyrzTqf3FYymriwKSJAnKbWyAK9c9WdIi3gn6O9Y8rOKc1tpSJGmQ00RU6pFiTC">
+        <stripe-card-number></stripe-card-number>
+        <stripe-card-expiry></stripe-card-expiry>
+        <stripe-card-cvc></stripe-card-cvc>
+        <stripe-submit-error></stripe-submit-error>
+        <stripe-submit><button>Submit</button></stripe-submit>
+    </stripe-form>
+`,a=l.html`
+    <stripe-form stripe-public-key="pk_test_51IG332FIx1iLyZGc71gYaVCUDKMPAFlRn49tyrzTqf3FYymriwKSJAnKbWyAK9c9WdIi3gn6O9Y8rOKc1tpSJGmQ00RU6pFiTC">
+        <stripe-card></stripe-card>
+        <stripe-submit-error></stripe-submit-error>
+        <stripe-submit><button>Submit</button></stripe-submit>
+    </stripe-form>
+`,c={};function b({components:e,...t}){return(0,o.mdx)("wrapper",{...c,...t,components:e,mdxType:"MDXLayout"},(0,o.mdx)("div",null,"Example 1"),(0,o.mdx)(m.ReactHtmlRender,{html:u,mdxType:"ReactHtmlRender"}),(0,o.mdx)("div",null,"Example 2"),(0,o.mdx)(m.ReactHtmlRender,{html:a,mdxType:"ReactHtmlRender"}),(0,o.mdx)("hr",null),(0,o.mdx)("div",null,"Proceed to PaymentClient and PaymentClient providers documentations to understand how to send stripe data to your api"))}b.isMDXComponent=!0}));
+//# sourceMappingURL=stripe-form.df73c490.js.map
